63bee2e67667ac054ef8a7112fa6fc136fa69904
Markus Sigg <dev.siggie@gmail.com>
2021-02-14 00:16:37 +0100

    Add fn nav keys to all boards.

--- keyboards/converter/adb_usb/keymaps/siggie0815/keymap.c
+++ keyboards/converter/adb_usb/keymaps/siggie0815/keymap.c
@@ -26,8 +26,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,
     KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,
     KC_TRNS,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,          KC_NO,                                  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-    KC_TRNS,XXXXXXX,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,          KC_TRNS,            KC_NO,              TO(0),  TO(2),  TO(4),
-    KC_TRNS,KC_TRNS,KC_TRNS,                        KC_NO,                                          KC_TRNS,KC_TRNS,    KC_NO,  KC_NO,  KC_NO,      KC_NO,          KC_NO,  KC_NO
+    KC_TRNS,XXXXXXX,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,          KC_TRNS,            KC_PGUP,            TO(0),  TO(2),  TO(4),
+    KC_TRNS,KC_TRNS,KC_TRNS,                        KC_NO,                                          KC_TRNS,KC_TRNS,    KC_HOME,KC_PGDN,KC_END,     KC_NO,          KC_NO,  KC_NO
     ),
 
     // AEK II ISO - L1
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS,    KC_DEL, KC_END, KC_PGDN,    KC_P7,  KC_P8,  KC_P9,  KC_PMNS,
     KC_LCAP,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,        KC_ENT,                                 KC_P4,  KC_P5,  KC_P6,  KC_PPLS,
     KC_LSFT,KC_NUBS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,        KC_RSFT,            KC_UP,              KC_P1,  KC_P2,  KC_P3,
-    KC_LCTL,KC_LALT,KC_LGUI,                        KC_SPC,                                         KC_RALT,KC_RCTL,    KC_LEFT,KC_DOWN,KC_RGHT,    KC_P0,          KC_PDOT,KC_PENT
+    KC_LCTL,KC_LALT,LT(3,KC_LGUI),                  KC_SPC,                                         KC_RALT,KC_RCTL,    KC_LEFT,KC_DOWN,KC_RGHT,    KC_P0,          KC_PDOT,KC_PENT
     ),
     // AEK II ISO - L2
     [3] = LAYOUT_ext_all(
@@ -45,8 +45,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,
     KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,
     KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,          KC_NO,                                  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,          KC_NO,              KC_NO,              TO(0),  TO(2),  TO(4),
-    KC_NO,  KC_NO,  KC_NO,                          KC_NO,                                          KC_NO,  KC_NO,      KC_NO,  KC_NO,  KC_NO,      KC_NO,          KC_NO,  KC_NO
+    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,          KC_NO,              KC_PGUP,            TO(0),  TO(2),  TO(4),
+    KC_NO,  KC_NO,  KC_NO,                          KC_NO,                                          KC_NO,  KC_NO,      KC_HOME,KC_PGDN,KC_END,     KC_NO,          KC_NO,  KC_NO
     ),
 
     // M0118D - L1
@@ -65,6 +65,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,  DE_AT,  KC_NO,  DE_EURO,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  DE_LBRC,DE_RBRC,DE_TILD,DE_BSLS,    XXXXXXX,XXXXXXX,XXXXXXX,    KC_NO,  KC_NO,  KC_NO,  KC_NO,
     KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  DE_LCBR,DE_RCBR,        KC_NO,                                  KC_NO,  KC_NO,  KC_NO,  KC_NO,
     KC_RSFT,DE_PIPE,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,          XXXXXXX,            KC_PGUP,            TO(0),  TO(2),  TO(4),
-    KC_RCTL,KC_RALT,KC_TRNS,                        KC_NO,                                          XXXXXXX,XXXXXXX,    KC_HOME,KC_END,KC_PGDN,     KC_NO,          KC_NO,  KC_NO
+    KC_RCTL,KC_RALT,KC_TRNS,                        KC_NO,                                          XXXXXXX,XXXXXXX,    KC_HOME,KC_END, KC_PGDN,    KC_NO,          KC_NO,  KC_NO
     ),
 };
